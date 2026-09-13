5cd10b384afda3f7157959bbde78a8f3b8145eec
Kevin Looper <kevin@kevinlooper.com>
2026-09-13 07:54:24 +0200

    feat: add welcome message to user shell

--- docker/terminal_config.sh
+++ docker/terminal_config.sh
@@ -10,3 +10,17 @@ source /workspaces/URC-2027/ros2_ws/install/setup.bash
 bind 'set show-all-if-ambiguous on'
 # Navigate to ros2_ws
 cd ros2_ws
+
+# Dynamic welcome message based on the active Pixi environment
+if [ -n "$PIXI_ENVIRONMENT_NAME" ]; then
+  echo -e "\e[1;32m➜ Active Pixi Environment: $PIXI_ENVIRONMENT_NAME\e[0m"
+else
+echo "========================================="
+echo "          Project BILL-EE 2027           "
+echo "========================================="
+echo "To get started, activate an environment:"
+echo "  $ pixi shell              | Default x86 Environment"
+echo "  $ pixi shell -e mac-cpu   | Apple-Silicon Mac Environment"
+echo "  $ pixi shell -e l4t       | Jetson Environment"
+echo ""
+fi
